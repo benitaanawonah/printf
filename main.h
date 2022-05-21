055df59cd90f2612cd5bc196f63b42ee1baeca0e
Benita Anawonah <benitaanawonah33@gmail.com>
2022-05-21 19:26:31 +0100

    main.h

--- main.h
+++ main.h
@@ -1,71 +1,32 @@
-#ifndef _FUNCTION_PRINTF_H_
-#define _FUNCTION_PRINTF_H_
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
-#include <stdlib.h>
-#include <stddef.h>
-
-#define BUFFER_SIZE 1024
-
 /**
- * struct modifier - mofifier fields collection
- * @flags: flags field composed of ['0', ' ', '#', '+', '-']
- * @width: width field, positive number
- * @precision: precision field positive number not including '.'
- * or -1 for '*'
- * @length: length field string composed of ['h', 'l']
- * @specifier: specifier character can one of
- * ['c', 's', '%', 'd', 'i', 'b', 'u', 'o', 'x', 'X', 'S', 'p', 'r', 'R']
- *
- */
-typedef struct modifier
-{
-	char *flags;
-	int width;
-	int precision;
-	char *length;
-	char specifier;
-} modifier_t;
-
-void *_realloc(void *ptr, unsigned int, unsigned int);
-int _putchar(char c);
-char *print_binary(modifier_t *, va_list);
-char *print_unsigned_int(modifier_t *, va_list);
-char *print_octal(modifier_t *, va_list);
-char *print_hex(modifier_t *, va_list);
-char *print_char(modifier_t *, va_list ap);
-char *print_int(modifier_t *, va_list ap);
-char *print_string(modifier_t *modif, va_list ap);
-char *print_rev(modifier_t *, va_list ap);
-char *print_big_s(modifier_t *, va_list);
-char *print_pointer(modifier_t *, va_list);
-char *rot13(char *s);
-int _strlen(char *s);
-char *print_rot(modifier_t *, va_list ap);
-
-void free_modifier(modifier_t *);
-char *get_flags(const char *, unsigned int *);
-int get_width(const char *, unsigned int *);
-int get_precision(const char *, unsigned int *);
-char *get_length(const char *, unsigned int *);
-char get_specifier(const char *, unsigned int *);
-modifier_t *get_modifier(const char *, unsigned int *);
-char *treat_format(const char *, unsigned int *, va_list);
-int _printf(const char *format, ...);
-char *_strcpy(char *dest, char *src);
-void array_rev(char *arr, int len);
-int int_len(int num);
-char *ito(int n);
-char *reverse(char *s);
-
-/**
- * struct print - multiple choice print
- * @f: char Type of print
- * @func: funct
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
 typedef struct print
 {
-	char f;
-	char *(*func)(modifier_t *, va_list);
-} t_print;
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
 #endif
